0538f750bbb9f981865b1c2bfb028c9c30c2b2d5
Aman Agarwal <agaraman0@gmail.com>
2019-10-30 10:17:03 +0530

    Single Number Problem (LTC)
    Given a non-empty array of integers, every element appears twice except for one. Find that single one.

---
+++ Array Important Algos And Ques/singleNum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        x = 0
+        
+        for i in nums:
+            x = x^i
+        
+        return x
+        
